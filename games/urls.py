5526cc21f33ad8a45d87bd45d1e44e55cb9ba534
Jiin Park <demon0315@naver.com>
2021-01-21 23:28:25 +0900

    game_delete urls and views

--- games/urls.py
+++ games/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("list/", views.game_list, name="game_list"),
     path("list/<int:pk>/detail", views.game_detail, name="game_detail"),
     path("list/<int:pk>/counter", views.game_counter, name="game_counter"),
+    path("list/<int:pk>/delete", views.game_counter, name="game_counter"),
 ]
