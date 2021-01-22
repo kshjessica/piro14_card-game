ac4678edff0b1e1f31daf7e060236d2643e76268
seoljiwon <suljiwon@naver.com>
2021-01-22 16:49:46 +0900

    attack and counter

--- games/views.py
+++ games/views.py
@@ -13,15 +13,28 @@ def game_main(request):
 @login_required
 def game_attack(request):
     if request.method == "POST":
-        pass
+        host=request.user
+        guest_name=request.POST.get('guest')
+        host_card=request.POST.get('card_number')
+        get_guest=User.objects.get(username=guest_name)
+        game=Game(host=host, guest=get_guest, host_card=host_card)
+        game.save()
+        return redirect('games:game_detail', pk=game.pk)
+    
     else:
-        card_numbers = list(range(1, 11))
-        print(card_numbers)
+        card_numbers = []
+        while len(card_numbers) < 5:
+            temp=random.randint(1, 10)
+            if temp not in card_numbers:
+                card_numbers.append(temp)
+        guests=list(User.objects.all())
+        guests.remove(request.user)
         ctx = {
-            "users": User.objects.all(),
-            "card_numbers": card_numbers,
+            'guests': guests,
+            'card_numbers': card_numbers,
         }
         return render(request, "games/game_attack.html", ctx)
+
 
 
 @login_required
@@ -39,7 +52,59 @@ def game_detail(request, pk):
 
 @login_required
 def game_counter(request, pk):
-    pass
+    game=Game.objects.get(pk=pk)
+    if request.method == "POST":
+        guest_card=request.POST.get('card_number')
+        game.guest_card=int(guest_card)
+        """
+        winner와 loser를 지정한다.
+        무승부일 경우 winner와 loser에는 아무 값도 할당되지 않는다.
+        무승부가 아닐 경우 winner.win과 loser.lose를 1 증가,
+        무승부일 경우 host와 guest 모두의 win을 1 증가시킨다.
+        """
+        host_card, guest_card = game.host_card, game.guest_card
+
+        if host_card != guest_card:  # 무승부가 아닐 때
+            GM_UP, GM_DOWN = 0, 1
+            gamemode = random.randint(0, 1)
+
+            if gamemode == GM_UP:
+                if host_card > guest_card:
+                    game.winner = game.host
+                    game.loser = game.guest
+                else:
+                    game.winner = game.guest
+                    game.loser = game.host
+            else:  # GM_DOWN
+                if host_card < guest_card:
+                    game.winner = game.host
+                    game.loser = game.guest
+                else:
+                    game.winner = game.guest
+                    game.loser = game.host
+
+            game.winner.win += 1
+            game.loser.lose += 1
+
+        else:  # 무승부일 때
+            game.host.win += 1
+            game.guest.win += 1
+
+        game.is_end = True
+        game.save()
+        return redirect('games:game_detail', pk=game.pk)
+    
+    else:
+        card_numbers = []
+        while len(card_numbers) < 5:
+            temp=random.randint(1, 10)
+            if temp not in card_numbers:
+                card_numbers.append(temp)
+        ctx = {
+            'pk':pk,
+            'card_numbers': card_numbers,
+        }
+        return render(request, "games/game_counter.html", ctx)
 
 
 @login_required
